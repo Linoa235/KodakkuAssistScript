e2c1c4a17b10087810bdd99e07455bd819df089f
Linoa235 <104484533+Linoa235@users.noreply.github.com>
2026-05-18 21:32:00 +0200

    Update Worqor Lar Dor.cs

--- ScriptTrad/Worqor Lar Dor.cs
+++ ScriptTrad/Worqor Lar Dor.cs
@@ -9,7 +9,7 @@ using KodakkuAssist.Module.Draw;
 
 namespace Cyf5119Script.Dawntrail.WorqorLarDor;
 
-[ScriptType(guid: "79f67f76-e205-42ba-b40e-435e0c356bc8", name: "Worqor Lar Dor", territorys: [1195], version: "0.0.0.3", author: "Linoa235")]
+[ScriptType(guid: "5ce75511-6313-46af-b60d-efcf3886d1c6", name: "Worqor Lar Dor", territorys: [1195], version: "0.0.0.3", author: "Linoa235")]
 public class WorqorLarDor
 {
     public void Init(ScriptAccessory accessory)
