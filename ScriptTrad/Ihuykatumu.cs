7abea438b2f8f64878b812add4a37734153501cf
Linoa235 <104484533+Linoa235@users.noreply.github.com>
2026-05-23 20:27:56 +0200

    Update Ihuykatumu.cs

--- ScriptTrad/Ihuykatumu.cs
+++ ScriptTrad/Ihuykatumu.cs
@@ -15,7 +15,7 @@ using KodakkuAssist.Module.Draw;
 
 namespace KodakkuScript.Script._07_DawnTrail;
 
-[ScriptType(guid: "3fff8fd8-a426-484b-a429-4ebbd9f6447c", name: "Ihuykatumu", territorys: [1167], version: "0.0.0.1",
+[ScriptType(guid: "539c4f5c-66ad-45dc-95e3-86f1889cb96b", name: "Ihuykatumu", territorys: [1167], version: "0.0.0.1",
     author: "Poetry")]
 public class Ihuykatumu
 {
